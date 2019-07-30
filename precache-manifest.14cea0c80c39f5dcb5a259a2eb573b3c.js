5b3b9ab8ef4601facf36c42b6888810bfedc7c3c
rizvan23 <nukhtaroff_rizvan@mail.ru>
2019-07-31 00:19:30 +0300

    Add files via upload

---
+++ precache-manifest.14cea0c80c39f5dcb5a259a2eb573b3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/kit/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "ee8a2c71e3bb418eb4f8",
+    "url": "/kit/static/js/runtime~main.ee8a2c71.js"
+  },
+  {
+    "revision": "c0be1e6f5ad35539a23c",
+    "url": "/kit/static/js/main.c0be1e6f.chunk.js"
+  },
+  {
+    "revision": "4e42bc6235b8d2201d1d",
+    "url": "/kit/static/js/2.4e42bc62.chunk.js"
+  },
+  {
+    "revision": "c0be1e6f5ad35539a23c",
+    "url": "/kit/static/css/main.1385173a.chunk.css"
+  },
+  {
+    "revision": "4e42bc6235b8d2201d1d",
+    "url": "/kit/static/css/2.09815868.chunk.css"
+  },
+  {
+    "revision": "ae5f252f0381e789245561656f8cd827",
+    "url": "/kit/index.html"
+  }
+];
